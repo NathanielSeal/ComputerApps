d6d3f8fc8ff97254080eaf9fd8c9d009592841c7
NathanielSeal <nathan.seal99@outlook.com>
2016-01-19 14:45:08 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+function onLoad() {
+            //alert("Function onLoad() called");
+            document.getElementById("timestamp").innerHTML = Date();
+            getAPIBadge()
+        }
+function checkNumber() {
+    var theNumber, theMessage;
+
+    // Get the value of the input field with id="numb"
+    theNumber = $('#smallnumber').val();
+
+    // If x is Not a Number or less than one or greater than 10
+    if (isNaN(theNumber) || theNumber < 1 || theNumber > 10) {
+        theMessage = "Number was expected to be between 1 and 10";
+    } else {
+        theMessage = "Number is Good";
+    }
+    $('#numberMessage').text(theMessage);
+}
+function getAPIBadge() {
+    var ctcAPI = "http://ChooseToCodeAPI.azurewebsites.net/api/values/";
+    $.post( ctcAPI, { 
+        SchoolName:"Bearden High School", 
+        ZipCode: "37919", 
+        Level:"Beginner"
+    }).done(function( data ) {
+        $("#badge").html(data);
+    });
+}
